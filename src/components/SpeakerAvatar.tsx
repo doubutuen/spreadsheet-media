64a291a40a742cf1e07c0675ca486e64ddf3ee3b
maedaouka <29334692+maedaouka@users.noreply.github.com>
2026-01-04 06:22:52 -0500

    Fix avatar image position in chat bubbles

--- src/components/SpeakerAvatar.tsx
+++ src/components/SpeakerAvatar.tsx
@@ -141,7 +141,7 @@ export default function SpeakerAvatar({ speakerId, name, avatar, size = 40 }: Sp
         <img
           src={avatar}
           alt={name}
-          style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+          style={{ width: '100%', height: '100%', objectFit: 'cover', objectPosition: 'center top' }}
         />
       </div>
     );
